4cdac424134376460886c34a86a9dbb3d9f0b1bc
Nicky Lause <Niclausel@gmail.com>
2024-09-27 19:09:06 +0300

    Encapsulation

---
+++ OOP/Encapsulation/BadBankAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Encapsulation
+{
+    public class BadBankAccount
+    {
+        public decimal balance;
+    }
+}
---
+++ OOP/Encapsulation/BankAccount.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Encapsulation
+{
+    /**
+     * ENCAPSULATION 
+     * In this BankAccount Class, it encapsulates the Deposit,withdrawal and GetBalance methods. The user will call Deposit without minding th logic in it
+     */
+    public class BankAccount
+    {
+        private decimal balance;
+        public BankAccount(decimal balance)
+        {
+            Deposit(balance);
+        }
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+
+
+        public void Deposit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Deposit amount MUST be a valid amount...");
+            }
+            balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount < 100)
+            {
+                throw new ArgumentException("The minimum withdrawal amount is Kes.100.");
+            }
+            if (balance < 100)
+            {
+                throw new ArgumentException("Insufficient funds");
+            }
+            balance -= amount;
+        }
+    }
+}
